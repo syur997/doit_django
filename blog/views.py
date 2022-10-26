55ccb51aac916bd787fe75b500f9929389f94035
yurim <syur997@naver.com>
2022-10-26 18:19:08 +0900

    CBV로 블로그 포스트 상세페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,32 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
+#def index(request):
+#    posts = Post.objects.all().order_by('-pk')
     
     # 필요한 코드 넣기
     # render(request, '템플릿 파일', {'키':변수값}) -> 3개 필수로 넣어야함
-    return render(
-        request,
-        'blog/index.html',
-        {'posts': posts})
+#    return render(
+#        request,
+#        'blog/index.html',
+#        {'posts': posts})
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+#def single_post_page(request, pk):
+#    post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {'post':post}
-    )
+#    return render(
+#        request,
+#        'blog/single_post_page.html',
+#        {'post':post}
+#    )
     # 파이썬 파일을 hmtl로 보내야하니까 렌더링해라
