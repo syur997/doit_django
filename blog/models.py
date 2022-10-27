b5ef9fca15dcc03d3dd65ab0a7c50c535e2d70c2
yurim <syur997@naver.com>
2022-10-27 14:00:07 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
@@ -10,6 +11,7 @@ class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성 예정
